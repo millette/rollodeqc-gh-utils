e8d01d9538027c4b058b158625c61a3a5582e12c
Robin Millette <robin@millette.info>
2016-03-28 19:42:20 -0400

    works, with tests

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 'use strict'
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string')
-  }
 
-  opts = opts || {}
+// npm
+const ghGot = require('gh-got')
+const pickBy = require('lodash.pickby')
 
-  return str + ' & ' + (opts.postfix || 'rainbows')
+const headersPicker = (value, key) =>
+  !key.indexOf('x-ratelimit-') ||
+  ['link', 'server', 'date', 'status'].indexOf(key) !== -1
+
+const chosenHeaders = function (headers) {
+  const picks = pickBy(headers, headersPicker)
+  picks.timestamp = new Date(picks.date).getTime()
+  picks.timestampDiff = Math.round((picks.timestamp - Date.now()) / 10) / 100
+  picks.timestamp /= 1000
+  picks['x-ratelimit-limit'] = parseInt(picks['x-ratelimit-limit'], 10)
+  picks['x-ratelimit-remaining'] = parseInt(picks['x-ratelimit-remaining'], 10)
+  picks['x-ratelimit-reset'] = parseInt(picks['x-ratelimit-reset'], 10)
+  picks.statusCode = parseInt(picks.status, 10)
+  return picks
 }
+
+exports.got = (searchUrl, obj) => ghGot(searchUrl, obj)
+  .then((result) => {
+    result.body.headers = chosenHeaders(result.headers)
+    return result.body
+  })
+
+exports.rateLimit = () => exports.got('rate_limit')
+  .then((result) => {
+    delete result.resources
+    return result
+  })
--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+/*eslint arrow-parens: [2, "as-needed"]*/
+
 'use strict'
 import test from 'ava'
 import fn from './'
 
-test('title', (t) => {
-  t.is(fn('unicorns'), 'unicorns & rainbows')
+test('rate limit', async t => {
+  const result = await fn.rateLimit()
+  t.is(result.headers.statusCode, 200)
+  t.is(result.rate.limit, 5000)
 })
